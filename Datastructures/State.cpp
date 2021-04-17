7dc77b20cc456f095a272408ad408d1a55403808
dries.demaeseneer <dries.demaeseneer@student.uantwerpen.be>
2021-04-17 12:39:55 +0200

    state prep

--- Datastructures/State.cpp
+++ Datastructures/State.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "State.h"
+
+State *State::operator[](std::string action) const {
+    for(auto it:action_to_state){
+        if(it.first == action){
+            return it.second;
+        }
+    }
+    return nullptr;
+}
+
+double State::operator[](State *state) const {
+    for(auto& it:state_to_probability){
+        if(&it.first == &state){
+            return it.second;
+        }
+    }
+    return 0;
+}
--- Datastructures/State.h
+++ Datastructures/State.h
@@ -5,8 +5,26 @@
 #ifndef TO_GROEP_STATE_H
 #define TO_GROEP_STATE_H
 
+#include <vector>
+#include <string>
+#include <iostream>
+
 
 class State {
+    // list of all actions
+    std::vector<std::string> actions;
+    // list of action-state pairs
+    std::vector<std::pair<std::string, State*>> action_to_state;
+    // list of state-probability pairs
+    std::vector<std::pair<State*, double>> state_to_probability;
+    // list of state-reward pairs
+    std::vector<std::pair<State*, double>> state_to_reward;
+
+public:
+    // find corresponding state to an action
+    State* operator[](std::string action) const;
+    // find corresponding probability to a state //TODO: probability or reward?
+    double operator[](State* state) const;
 
 };
 
