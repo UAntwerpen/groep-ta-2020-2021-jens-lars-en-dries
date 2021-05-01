e0e69099f48e9c46e13aea025286c61ae43ee6e5
LarsDeLeeuw <deleeuwlars@icloud.com>
2021-05-01 09:03:40 +0200

    CMakeLists for parser lib, small update LAParser

--- parser/LAParser.cpp
+++ parser/LAParser.cpp
@@ -1,1 +1,5 @@
 #include "LAParser.h"
+
+bool LAParser::parseFile(string inputFile, LA *parsing_LA) {
+    return false;
+}
--- parser/LAParser.h
+++ parser/LAParser.h
@@ -3,10 +3,12 @@
 
 
 #include "AbstractParser.h"
+#include "tinyxml/tinyxml.h"
+#include "../datastructures/LA.h"
 
 class LAParser : public AbstractParser {
 public:
-
+    bool parseFile(string inputFile, LA* parsing_LA);
 private:
 
 };
