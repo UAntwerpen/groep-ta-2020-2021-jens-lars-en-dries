7efbc10b9b4c758ca321d802125cfa1085164cdd
LarsDeLeeuw <larsdeleeuw@users.noreply.github.com>
2021-05-08 10:44:23 +0200

    DID: Better error descriptions.

--- parser/AbstractParser.cpp
+++ parser/AbstractParser.cpp
@@ -17,9 +17,47 @@ void AbstractParser::loadFile(string filename) {
     if(!(FileExists(filename))) _assert ("File to parse does not exist.", __FILE__, __LINE__);
     fDoc.LoadFile(filename.c_str());
     if(fDoc.Error()){
-        // TODO: Better error descriptions.
-        string error = string("There has been an error parsing file:" + filename);
-        _assert(error.c_str(), __FILE__, __LINE__);
+        std::string compareStr = std::string(fDoc.ErrorDesc());
+        if("Failed to open file" == compareStr){
+            string error = string("Failed to open file:" + filename);
+            _assert(error.c_str(), __FILE__, __LINE__);
+        }
+        else if("Error parsing Element." == compareStr){
+            string error = string("Error parsing Element.");
+            _assert(error.c_str(), __FILE__, __LINE__);
+        }
+        else if("Failed to read Element name" == compareStr){
+            string error = string("Failed to read Element name");
+            _assert(error.c_str(), __FILE__, __LINE__);
+        }
+        else if("Error reading Element value." == compareStr){
+            string error = string("Error reading Element value.");
+            _assert(error.c_str(), __FILE__, __LINE__);
+        }
+        else if("Error reading Attributes." == compareStr){
+            string error = string("Error reading Attributes.");
+            _assert(error.c_str(), __FILE__, __LINE__);
+        }
+        else if("Error: empty tag." == compareStr){
+            string error = string("Error: empty tag.");
+            _assert(error.c_str(), __FILE__, __LINE__);
+        }
+        else if("Error reading end tag." == compareStr){
+            string error = string("Error reading end tag.");
+            _assert(error.c_str(), __FILE__, __LINE__);
+        }
+        else if("Error parsing Comment." == compareStr){
+            string error = string("Error parsing Comment.");
+            _assert(error.c_str(), __FILE__, __LINE__);
+        }
+        else if("Error document empty." == compareStr){
+            string error = string("Error document empty.");
+            _assert(error.c_str(), __FILE__, __LINE__);
+        }
+        else{
+            string error = string("Generic error occurred while loading file.");
+            _assert(error.c_str(), __FILE__, __LINE__);
+        }
     }
     fRoot = fDoc.FirstChildElement();
 }
